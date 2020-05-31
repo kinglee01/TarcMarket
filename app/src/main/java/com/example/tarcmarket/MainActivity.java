d3f736228db53b0503a06e316fe74fa6514ec776
kinglee01 <zhenbolee59@gmail.com>
2020-05-31 21:27:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tarcmarket/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.tarcmarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    private Button registerButton, loginButton;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        registerButton = (Button) findViewById(R.id.register_button);
+        loginButton = (Button) findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this , LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this , RegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
